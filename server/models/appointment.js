e0b96f22ab9a4d5d54e3f5c266d27ea6b718b404
rongerard <ron.ronscez@gmail.com>
2024-09-24 10:06:44 +0800

    lol

--- server/models/appointment.js
+++ server/models/appointment.js
@@ -1,118 +1,67 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-const secretary = require('./secretary');
-const patient = require('./patient');
-const schedule = require('./schedule');
-const doctor = require('./doctor');
+module.exports = (sequelize, DataTypes) => {
+    const Appointment = sequelize.define('Appointment', {
+        // your column definitions
+        FIRST_NAME: { type: DataTypes.STRING, allowNull: false },
+        MIDDLE_NAME: { type: DataTypes.STRING },
+        LAST_NAME: { type: DataTypes.STRING, allowNull: false },
+        AGE: { type: DataTypes.INTEGER, allowNull: false },
+        DOCTOR_NAME: { type: DataTypes.STRING, allowNull: false },
+        APPOINTMENT_TIME: { type: DataTypes.STRING, allowNull: false },
+        APPOINTMENT_DATE: { type: DataTypes.DATE, allowNull: false },
+        REASON: { type: DataTypes.STRING, allowNull: false },
+        CONTACT_NUMBER: { type: DataTypes.STRING, allowNull: false },
+        TYPE: { type: DataTypes.STRING, defaultValue: false },
+        STATUS: { type: DataTypes.STRING, allowNull: false },
+        SCHEDULE_ID: { type: DataTypes.INTEGER, allowNull: false },
+        SECRETARY_ID: { type: DataTypes.INTEGER },
+        DOCTOR_ID: { type: DataTypes.INTEGER, allowNull: false },
+        PATIENT_ID: { type: DataTypes.INTEGER },
+        createdAt: { type: DataTypes.DATE, allowNull: false },
+        updatedAt: { type: DataTypes.DATE, allowNull: false }
+    });
 
-const Appointment = sequelize.define('Appointment', {
-    FIRST_NAME: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    MIDDLE_NAME: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    LAST_NAME: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    AGE: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    DOCTOR_NAME: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    APPOINTMENT_TIME: {  // Corrected typo from AppoinmentTime to AppointmentTime
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    APPOINTMENT_DATE: {
-        type: DataTypes.DATEONLY,
-        allowNull: false
-    },
-    REASON: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    CONTACT_NUMBER: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    TYPE: {
-        type: DataTypes.STRING,
-        defaultValue: false
-    },
-    STATUS: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    SCHEDULE_ID: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    }
-});
+    // Define associations with foreign key constraints
+    Appointment.associate = function (models) {
+        Appointment.belongsTo(models.Schedule, {
+            foreignKey: {
+                name: 'fk_appointment_schedule',
+                field: 'SCHEDULE_ID',
+                allowNull: false
+            },
+            onDelete: 'NO ACTION',
+            onUpdate: 'CASCADE'
+        });
 
-secretary.hasMany(Appointment, {
-    foreignKey: {
-        name: 'SECRETARY_ID',
-        allowNull: true
-    }
-});
-Appointment.belongsTo(secretary, {
-    foreignKey: {
-        name: 'SECRETARY_ID',
-        allowNull: true
-    }
-});
+        Appointment.belongsTo(models.Secretary, {
+            foreignKey: {
+                name: 'fk_appointment_secretary',
+                field: 'SECRETARY_ID',
+                allowNull: true
+            },
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE'
+        });
 
-Appointment.belongsTo(schedule, {
-    foreignKey: {
-        name: 'SCHEDULE_ID',
-        allowNull: false
-    }
-});
-schedule.hasMany(Appointment, {
-    foreignKey: {
-        name: 'SCHEDULE_ID',
-        allowNull: false
-    }
-});
+        Appointment.belongsTo(models.Doctor, {
+            foreignKey: {
+                name: 'fk_appointment_doctor',
+                field: 'DOCTOR_ID',
+                allowNull: false
+            },
+            onDelete: 'NO ACTION',
+            onUpdate: 'CASCADE'
+        });
 
+        Appointment.belongsTo(models.Patient, {
+            foreignKey: {
+                name: 'fk_appointment_patient',
+                field: 'PATIENT_ID',
+                allowNull: true
+            },
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE'
+        });
+    };
 
-Appointment.belongsTo(doctor, {
-    foreignKey: {
-        name: 'DOCTOR_ID',
-        allowNull: false
-    }
-});
-doctor.hasMany(Appointment, {
-    foreignKey: {
-        name: 'DOCTOR_ID',
-        allowNull: false
-    }
-});
-
-
-// A patient can have many appointments
-patient.hasMany(Appointment, {
-    foreignKey: {
-        name: 'PATIENT_ID',
-        allowNull: true
-    }
-});
-
-// An appointment belongs to one patient
-Appointment.belongsTo(patient, {
-    foreignKey: {
-        name: 'PATIENT_ID',
-        allowNull: true
-    }
-});
-
-
-
-module.exports = Appointment;  // Corrected from modules.exports to module.exports
+    return Appointment;
+};
